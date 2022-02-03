5accafbdf5bfb404991cc4347d83d2653dd2af05
Lynn Simonen <lynnsimonen@yahoo.com>
2022-02-02 23:48:42 -0600

    Mod 2 Assignment

--- wwwroot/js/balloons.js
+++ wwwroot/js/balloons.js
@@ -1,3 +1,3 @@
 $(function(){
-    $('#birthday').pickadate({ format: 'mmmm, d' });
+    $('#birthday').pickadate({ format: 'd mmmm' });
 });
--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -4,12 +4,36 @@ $(function(){
 
     $('.code').on('click', function(e) {
         e.preventDefault();
-        // first pause the audio (in case it is still playing)
-        toast.pause();
-        // reset the audio
-        toast.currentTime = 0;
+         // first pause the audio (in case it is still playing)
+         toast.pause();
+         // reset the audio
+         toast.currentTime = 0;
         // play audio
         toast.play();
         $('#toast').toast({ autohide: false }).toast('show');
+
+     // Display the product name and discount code in the toast (use data- attributes)
+     $('#product').html($(this).data('product'));
+     $('#code').html($(this).data('code'));    
+
     });
+ 
+
+    
 });
+
+
+//------------------
+
+// <!-- toast -->
+//     <div id="toast" class="toast">
+//         <div class="toast-header">
+//             <strong class="mr-auto" id="product">Product Name</strong>
+//             <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+//                 <span aria-hidden="true">&times;</span>
+//             </button>
+//         </div>
+//         <div class="toast-body">
+//             Discount Code: <strong id="code">CODE</strong>
+//         </div>
+//     </div>
