d40100049fa421849282d0b98a46299a30ea2c49
Lynn Simonen <lynnsimonen@yahoo.com>
2022-02-03 09:50:15 -0600

    Mod3 Class Period Updates - All Working

--- wwwroot/js/balloons.js
+++ wwwroot/js/balloons.js
@@ -1,3 +1,32 @@
 $(function(){
     $('#birthday').pickadate({ format: 'd mmmm' });
+
+    
+    // uncheck all checkboxes (FireFox)
+    $('.form-check-input').each(function () {
+        $(this).prop('checked', false);
+    });
+
+     // event listener for check/uncheck
+     $('.form-check-input').on('change', function () {
+        // make the image visible
+        $('#' + this.id + 'Img').css('visibility', 'visible')
+        // animate balloon in/out based on checkbox
+        $(this).is(':checked') ?
+         $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown') :
+         $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
+    });
+
+    // //Trigger live toast if no balloon checkboxes are checked
+    // var toastTrigger = document.getElementById('submit')
+    // var toastLiveExample = document.getElementById('liveToast')
+    // if (toastTrigger) {
+    //     toastTrigger.addEventListener('click', function () {
+    //     var toast = new bootstrap.Toast(toastLiveExample)
+
+    //     toast.show()
+    // })
+    // }
+
+
 });
