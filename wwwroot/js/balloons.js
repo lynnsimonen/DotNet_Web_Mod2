52572722180947c8458635ec3f2886bd35dea99b
Lynn Simonen <lynnsimonen@yahoo.com>
2022-02-08 20:04:40 -0600

    Changes

--- wwwroot/js/balloons.js
+++ wwwroot/js/balloons.js
@@ -13,7 +13,7 @@ $(function(){
         $('#' + this.id + 'Img').css('visibility', 'visible')
         // animate balloon in/out based on checkbox
         $(this).is(':checked') ?
-         $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown') :
+         $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown'):
          $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
     });
 
@@ -21,11 +21,11 @@ $(function(){
     //Choose a random animate.css attention seeker and apply when the page loads using JavaScript.    
     var arr = ['bounce','pulse','flash','swing','tada','jello','wobble'];
     var randomarr = arr[Math.floor(Math.random()*arr.length)];
-    $('#happybday').removeClass().addClass('animate__animated animate__' + randomarr)
+    $('#happybday').removeClass().addClass('animate__animated animate__' + randomarr);
     //$('#array').html(arr.toString());
     
 
-        //Assign03 - 2. Trigger live toast if no balloon checkboxes are checked
+    //Assign03 - 2. Trigger live toast if no balloon checkboxes are checked
      $('#submit').on('click', function(e) {
         var isChecked = false;
         // determine if any check boxes are checked
@@ -39,21 +39,35 @@ $(function(){
         }
     });
 
-    //Assign03 - 3. Add ability to check / uncheck all balloons with a single click (using a button, checkbox, link, etc…)
-    $('#checkall').click(function(){
+     //Assign03 - 3. Add ability to check / uncheck all balloons with a single click (using a button, checkbox, link, etc…)
+     $('#checkall').click(function(){
         $('input[type=checkbox]').prop("checked",$('#checkall').is(':checked'));    
         });
 
-
-    //Assign01 - 4. Hovering the mouse over a checkbox label should change the color of the h1 element (Happy Birthday!) 
+    //Assign03 - 4. Hovering the mouse over a checkbox label should change the color of the h1 element (Happy Birthday!) 
     //to the balloon color indicated in the label. Moving the mouse out of the label should reset the color.
+    $('.form-check-label').hover(
+        function () {
+            var colorname = $(this).attr("for");
+            $('h1').css({"color": colorname});
+        },
+        function(){
+            $('h1').css({"color": "black"});
+        }
+    );
 
 });
 
-
-
    /*  $(document).ready(function(){
        
+    }); */
+
+    /* $('.form-check-label').hover(function () {
+        var colorname = $(this).attr("for");
+        //var hello = "hello there";
+        //alert(colorname);
+        $('h1').css("color", colorname);
+        //$('#happybday').css("color", "green");
     }); */
 
 
